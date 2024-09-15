16b4b577ed24f9ff5a2a24ad7b9faca2e9c88378
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-09-15 17:11:37 +0300

    fixed port name

--- index.js
+++ index.js
@@ -112,5 +112,5 @@ app.get("/info", (req, resp) => {
 const PORT = process.env.port || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running at PORT: ${port}/`);
+  console.log(`Server running at PORT: ${PORT}/`);
 });
